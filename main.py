668d2d6a165635c1f0c019abe908c821da43456c
Kento Seki <z5366832@ad.unsw.edu.au>
2024-11-15 22:10:45 +1100

    moving lasers and meteors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from random import randint
+from random import randint, uniform
 from os.path import join
 
 class Player(pygame.sprite.Sprite):
@@ -9,6 +9,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_frect(center=(WINDOW_WIDTH/2,WINDOW_HEIGHT - 150))
         self.direction = pygame.Vector2()
         self.speed = 300
+        # Laser cooldown
+        self.canShoot = True
+        self.laserShootTime = 0
+        self.coolDownDuration = 400
 
     def update(self, dt):
         # Update player position according to keyboard input
@@ -18,8 +22,18 @@ class Player(pygame.sprite.Sprite):
         self.direction = self.direction.normalize() if self.direction else self.direction
         self.rect.center += self.direction * self.speed * dt
 
-        if pygame.key.get_just_pressed()[pygame.K_SPACE]:
-            print('fire laser!')
+        if pygame.key.get_just_pressed()[pygame.K_SPACE] and self.canShoot:
+            Laser(laserSurface, self.rect.midtop, allSprites)
+            self.canShoot = False
+            self.laserShootTime = pygame.time.get_ticks()
+
+        self.laserTimer()
+
+    def laserTimer(self):
+        if not self.canShoot:
+            currentTime = pygame.time.get_ticks()
+            if currentTime - self.laserShootTime >= self.coolDownDuration:
+                self.canShoot = True
 
 class Star(pygame.sprite.Sprite):
     def __init__(self, group, surface):
@@ -32,9 +46,38 @@ class Star(pygame.sprite.Sprite):
     def update(self, dt):
         pass
 
-#
+class Laser(pygame.sprite.Sprite):
+
+    LASER_SPEED = 400
+
+    def __init__(self, surface, pos, group):
+        super().__init__(group)
+        self.image = surface
+        self.rect = self.image.get_frect(midbottom=pos)
+
+    def update(self, dt):
+        self.rect.centery -= self.LASER_SPEED * dt
+        # make sure lasers that go off the screen don't occupy memory forever
+        if self.rect.bottom < 0:
+            self.kill()
+
+class Meteor(pygame.sprite.Sprite):
+    METEOR_LIFETIME = 3000
+
+    def __init__(self, surface, pos, group):
+        super().__init__(group)
+        self.image = surface
+        self.rect = self.image.get_frect(center=pos)
+        self.timeCreated = pygame.time.get_ticks()
+        self.direction = pygame.Vector2(uniform(-0.5,0.5), 1)
+        self.speed = randint(400, 500)
+
+    def update(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+        if pygame.time.get_ticks() - self.timeCreated > self.METEOR_LIFETIME:
+            self.kill()
+
 # General setup
-#
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 displaySurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -42,21 +85,20 @@ pygame.display.set_caption('Space Shooter')
 running = True
 clock = pygame.time.Clock()
 
-allSprites = pygame.sprite.Group()
+# Import images as surfaces
 starSurface = pygame.image.load(join('images', 'star.png')).convert_alpha()
+meteorSurface = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+laserSurface = pygame.image.load(join('images', 'laser.png'))
+
+# Sprites
+allSprites = pygame.sprite.Group()
 for i in range(20):
     star = Star(allSprites, starSurface)
 player = Player(allSprites)
 
-#
-# Load images as surfaces
-#
-
-meteorSurface = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
-meteorRect = meteorSurface.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-
-laserSurface = pygame.image.load(join('images', 'laser.png'))
-laserRect = laserSurface.get_frect(bottomleft=(WINDOW_WIDTH-20, WINDOW_HEIGHT-20))
+# Meteor event which triggers regularly - this can be detected by event loop
+meteorEvent = pygame.event.custom_type()
+pygame.time.set_timer(meteorEvent, 500)
 
 #
 # Loop indefinitely, creating and displaying frames
@@ -68,6 +110,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == meteorEvent:
+            x, y = randint(0,WINDOW_WIDTH), randint(-200,-100)
+            Meteor(meteorSurface, (x,y), allSprites)
         #
         # Handling inputs via the event loop - possible, but not recommended
         #
